9db6bad03d0e2302a2a08d3286929a0307d42da5
No one knows <142857872+unknownbuddy1@users.noreply.github.com>
2023-12-24 18:16:53 +0530

    Update settings.py

--- Bikash/utils/inline/settings.py
+++ Bikash/utils/inline/settings.py
@@ -8,28 +8,28 @@ def setting_markup(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝐀𝐮𝐝𝐢𝐨 𝐐𝐮𝐚𝐥𝐢𝐭𝐲", callback_data="AQ"
+                text="Audio Quality", callback_data="AQ"
             ),
             InlineKeyboardButton(
-                text="𝐕𝐢𝐝𝐞𝐨 𝐐𝐮𝐚𝐥𝐢𝐭𝐲", callback_data="VQ"
+                text="Video Quality ", callback_data="VQ"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="𝐀𝐮𝐭𝐡 𝐔𝐬𝐞𝐫's", callback_data="AU"
+                text="Auth User's", callback_data="AU"
             ),
             InlineKeyboardButton(
-                text="𝐂𝐥𝐞𝐚𝐧 𝐌𝐨𝐝𝐞", callback_data="CM"
+                text="Clean Mode", callback_data="CM"
             ),
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="🍿Movies-Bot🍿", url=f"https://t.me/kissu_movies_bot"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="💖 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 💖", url=f"{SUPPORT_GROUP}",
+                text="💐 Group 💐", url=f"{SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
